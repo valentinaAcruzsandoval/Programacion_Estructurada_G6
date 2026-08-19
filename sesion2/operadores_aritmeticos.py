7a785ea266e2761712021ab372bc0e3530b227e1
Valentina <vacruz@uamv.edu.ni>
2026-08-19 10:52:41 -0600

    clase sesion 2

---
+++ sesion2/operadores_aritmeticos.py
@@ -0,0 +1,10 @@
+#Operadore aritmeticos
+#Leer 2 numeros y calcular: la suma, resta, multiplicacion, division y comprobar si el primero es multiplo de segundo
+number1 = int(input("Ingrese un numero: "))
+number2 = int(input("Ingrese otro numero: "))
+
+print(f"{number1} + {number2} = {number1 + number2}")
+print(f"{number1} - {number2} = {number1 - number2}")
+print(f"{number1} * {number2} = {number1 * number2}")
+print(f"{number1} / {number2} = {number1 / number2}")
+print(f"{number1} % {number2} = {number1 % number2}")
